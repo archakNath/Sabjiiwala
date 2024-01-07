82102a0cb4cc1682ae7ac6841f7c3c49274df2a6
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-01-07 21:10:49 +0530

    changes

--- assets/js/universal.js
+++ assets/js/universal.js
@@ -28,17 +28,3 @@ const nav_shop = document.querySelector('nav ul a:nth-child(2)');
 nav_shop.onclick = () => {
     localStorage.setItem("category", 'all');
 }
-
-const nav = document.querySelector('header');
-function changeBackgroundColorOnScroll() {
-    if (window.scrollY > 150) {
-        // Change background color when scrolled
-        nav.style.backgroundColor = '#a4bfa7'; // Change to the desired color
-        nav.style.boxShadow = '0px 3px 10px rgba(0, 0, 0, 0.136)';
-    } else {
-        // Revert to the initial background color when at the top
-        nav.style.backgroundColor = '#a4bfa700'; // Change to the initial color
-        nav.style.boxShadow = 'none';
-    }
-}
-window.addEventListener('scroll', changeBackgroundColorOnScroll);
