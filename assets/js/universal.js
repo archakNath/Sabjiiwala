776e2219690b20b5f8f10cee3fa76a6f818f54bf
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-01-08 19:15:13 +0530

    fixed nav bgcolor

--- assets/js/universal.js
+++ assets/js/universal.js
@@ -3,18 +3,25 @@
 const navbar = document.getElementById("nav-list");
 const menubtn = document.getElementById("menu-button");
 const logo = document.getElementById("logo")
+const header = document.querySelector('header');
 
 menubtn.onclick = () => {
     var nav_visible = window.getComputedStyle(navbar).display;
-    var screenWidth = window.screen.width;
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
     if (nav_visible == "none") {
         navbar.style.display = "block";
         logo.style.display = "none";
+        if (screenWidth < 700) {
+            header.style.backgroundColor = '#a4bfa7';
+        }
         menubtn.src = "/assets/images/icons/cancel.png"
     } else {
         navbar.style.display = "none";
         logo.style.display = "block";
+        if (screenWidth < 700 && window.scrollY <= 150) {
+            header.style.backgroundColor = 'rgba(164, 191, 167, 0)';
+        }
         menubtn.src = "/assets/images/icons/menu.png"
     }
 }
