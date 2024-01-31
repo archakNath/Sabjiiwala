9f40abca9d40955cc9db35b7825934be116bca10
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-02-01 02:29:04 +0530

    order success animation

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,3 +2,13 @@ let customer_name = document.querySelector("#cards h3");
 if(localStorage.getItem("customer-name") != null){
     customer_name.textContent = localStorage.getItem("customer-name");
 }
+
+const lottie = document.getElementById("lottie");
+if(sessionStorage.getItem("placed") == "true"){
+    setTimeout(() => {
+        lottie.style.display = 'none';
+        sessionStorage.removeItem("placed");
+    }, 2000);
+} else {
+    lottie.style.display = 'none';
+}
